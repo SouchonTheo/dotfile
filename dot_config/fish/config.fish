2fa447a13c812b561d36373261b3d78dbe9079dd
Theo Souchon <theo.souchon@zama.ai>
2026-05-04 11:09:05 +0200

    chore: update after the work computer reboot

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-
+export PATH="$HOME/.local/bin:$PATH"
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
@@ -11,3 +11,10 @@ set -gx VISUAL nvim
 
 alias v="nvim"
 zoxide init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/theo/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
